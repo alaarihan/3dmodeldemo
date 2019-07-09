56cff2230429551e49aff94416c54eb8cfc40029
Alaa Rihan <alaa.addeen@gmail.com>
2019-07-10 02:22:20 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <threeDModel/>
+  </div>
+</template>
+
+<script>
+import threeDModel from "./components/threeDModel";
+
+export default {
+  name: "App",
+  components: {
+    threeDModel
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/threeDModel.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <model-obj src="T3Nano.obj" mtl="T3Nano.mtl"></model-obj>
+  </div>
+</template>
+
+<script>
+import { ModelObj } from "vue-3d-model";
+export default {
+  name: "threeDModel",
+  components: { ModelObj },
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
